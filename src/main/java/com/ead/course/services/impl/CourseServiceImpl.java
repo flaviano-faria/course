18a2819082746576def681d4738f61f7a54616f3
flaviano-faria <flsilv89@yahoo.com.br>
2025-07-24 20:22:44 -0300

    Implementing cascade in CourseServiceImpl

--- src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/CourseServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.repositories.CourseRepository;
 import com.ead.course.repositories.LessonRepository;
 import com.ead.course.repositories.ModuleRepository;
 import com.ead.course.services.CourseService;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -24,11 +26,18 @@ public class CourseServiceImpl implements CourseService {
         this.lessonRepository = lessonRepository;
     }
 
+    @Transactional
     @Override
     public void delete(CourseModel courseModel) {
         List<ModuleModel> moduleModelList =
                 moduleRepository.findModulesByCourse(courseModel.getCourseId());
         if(!moduleModelList.isEmpty()) {
+            for(ModuleModel moduleModel : moduleModelList) {
+                List<LessonModel> lessonModelList = lessonRepository.findAllLessonsByModule(moduleModel.getModuleId());
+                if(!lessonModelList.isEmpty()) {
+                    lessonRepository.deleteAll(lessonModelList);
+                }
+            }
             moduleRepository.deleteAll(moduleModelList);
         }
         courseRepository.delete(courseModel);
