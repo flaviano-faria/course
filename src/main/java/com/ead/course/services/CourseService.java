26558037fc4463b0308a139b2a9b276491ae9a98
flaviano-faria <flsilv89@yahoo.com.br>
2025-07-26 19:54:17 -0300

    Implementing createCourse method

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,16 @@
 package com.ead.course.services;
 
+import com.ead.course.dtos.CourseRecordDTO;
 import com.ead.course.models.CourseModel;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface CourseService {
 
     void delete(CourseModel courseModel);
+
+    CourseModel save(CourseRecordDTO courseRecordDTO);
+
+    boolean existsByName(@NotBlank String name);
 }
