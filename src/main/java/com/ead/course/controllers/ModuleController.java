3c0c1e991910ded8a7256615bb68f76323a89f87
flaviano-faria <flsilv89@yahoo.com.br>
2025-08-06 21:18:33 -0300

    Implementing new specification method moduleCourseId in SpecificationTemplate

--- src/main/java/com/ead/course/controllers/ModuleController.java
+++ src/main/java/com/ead/course/controllers/ModuleController.java
@@ -4,7 +4,9 @@ import com.ead.course.dtos.ModuleRecordDTO;
 import com.ead.course.models.ModuleModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.services.ModuleService;
+import com.ead.course.specifications.SpecificationTemplate;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,7 +38,10 @@ public class ModuleController {
 
     @GetMapping("/courses/{courseId}/modules")
     public ResponseEntity<List<ModuleModel>> getAllModules(
-            @PathVariable(value = "courseId") UUID courseId){
+            @PathVariable(value = "courseId") UUID courseId,
+            SpecificationTemplate.ModuleSpec spec,
+            Pageable pageable){
+
         return ResponseEntity.status(HttpStatus.OK)
                 .body(moduleService.findAllModulesIntoCourse(courseId));
     }
