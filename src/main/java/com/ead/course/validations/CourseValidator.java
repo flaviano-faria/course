822db438f5e255bb76e180bdec4d38b9a4df6b3c
flaviano-faria <flsilv89@yahoo.com.br>
2025-08-30 15:37:32 -0300

    Updating authuser and refactoring dtos, controllers and services

---
+++ src/main/java/com/ead/course/validations/CourseValidator.java
@@ -0,0 +1,68 @@
+package com.ead.course.validations;
+
+import com.ead.course.clients.AuthUserClient;
+import com.ead.course.dtos.CourseRecordDTO;
+import com.ead.course.dtos.UserRecordDto;
+import com.ead.course.enums.UserType;
+import com.ead.course.exceptions.GlobalExceptionHandler;
+import com.ead.course.services.CourseService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.util.UUID;
+
+@Component
+public class CourseValidator implements Validator {
+
+    Logger logger = LogManager.getLogger(CourseValidator.class);
+
+    private final Validator validator;
+    final CourseService courseService;
+    final AuthUserClient authUserClient;
+
+    public CourseValidator(@Qualifier("defaultValidator") Validator validator, CourseService courseService, AuthUserClient authUserClient) {
+        this.validator = validator;
+        this.courseService = courseService;
+        this.authUserClient = authUserClient;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return false;
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        CourseRecordDTO courseRecordDto = (CourseRecordDTO) o;
+        validator.validate(courseRecordDto, errors);
+        if(!errors.hasErrors()){
+            validateCourseName(courseRecordDto, errors);
+            validateUserInstructor(courseRecordDto.userInstructor(), errors);
+        }
+    }
+
+    private void validateCourseName(CourseRecordDTO courseRecordDto, Errors errors){
+        if(courseService.existsByName(courseRecordDto.name())){
+            errors.rejectValue("name", "courseNameConflict", "Course Name is Already Taken.");
+            logger.error("Error validation courseName: {} ", courseRecordDto.name());
+        }
+    }
+
+    private void validateUserInstructor(UUID userInstructor, Errors errors){
+        ResponseEntity<UserRecordDto> responseUserInstructor = authUserClient.getOneUserById(userInstructor);
+        if(responseUserInstructor.getBody().userType().equals(UserType.STUDENT) ||
+            responseUserInstructor.getBody().userType().equals(UserType.USER)){
+            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+            logger.error("Error validation userInstructor: {} ", userInstructor);
+        }
+    }
+
+
+
+
+}
