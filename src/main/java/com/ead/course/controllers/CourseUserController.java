67d45caa845db42427d9ca63898ee037cdb79697
flaviano-faria <flsilv89@yahoo.com.br>
2025-09-30 22:47:57 -0300

    Finishing SpecificationTemplate

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -3,6 +3,8 @@ package com.ead.course.controllers;
 import com.ead.course.dtos.SubscriptionRecordDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
+import com.ead.course.services.UserService;
+import com.ead.course.specifications.SpecificationTemplate;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -18,18 +20,22 @@ import java.util.UUID;
 public class CourseUserController {
 
     final CourseService courseService;
+    final UserService userService;
 
     public CourseUserController(
-                                CourseService courseService) {
+            CourseService courseService, UserService userService) {
         this.courseService = courseService;
+        this.userService = userService;
     }
 
     @GetMapping("/courses/{courseId}/{users}")
-    public ResponseEntity<Object> getAllUsersByCourse(@PageableDefault(sort = "userId",
+    public ResponseEntity<Object> getAllUsersByCourse(SpecificationTemplate.UserSpec spec,
+                                                       @PageableDefault(sort = "userId",
     direction = Sort.Direction.ASC) Pageable pageable, @PathVariable(value = "courseId") UUID courseId){
 
+        courseService.findById(courseId);
         return ResponseEntity.status(HttpStatus.OK)
-                .body(" ");
+                .body(userService.findAll(SpecificationTemplate.userCourseId(courseId).and(spec), pageable));
     }
 
     @PostMapping("/courses/{courseId}/users/subscription")
